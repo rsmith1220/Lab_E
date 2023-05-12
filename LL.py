422fb38dfdbb4c5ec8b91c7242713351f46d3408
Rebecca Smith <70491756+rsmith1220@users.noreply.github.com>
2023-05-11 22:15:17 -0600

    fixes

--- LL.py
+++ LL.py
@@ -9,3 +9,67 @@ def get_terminals_and_non_terminals(productions):
                     terminals.add(symbol)
 
     return terminals, non_terminals
+
+def first_sets(productions):
+    terminals, non_terminals = get_terminals_and_non_terminals(productions)
+    first = {non_terminal: set() for non_terminal in non_terminals}
+
+    changed = True
+    while changed:
+        changed = False
+        for non_terminal in non_terminals:
+            for production in productions[non_terminal]:
+                for symbol in production:
+                    if symbol in terminals:
+                        # For each terminal in the production, add it to the 'first' set
+                        if symbol not in first[non_terminal]:
+                            first[non_terminal].add(symbol)
+                            changed = True
+                        break
+                    else:
+                        # If the symbol is a non-terminal, add its 'first' set to the current non-terminal's 'first' set
+                        added = len(first[non_terminal])
+                        first[non_terminal].update(first[symbol] - {None})
+                        if len(first[non_terminal]) != added:
+                            changed = True
+                        if None not in first[symbol]:
+                            break
+                else:
+                    # If all symbols in the production can generate the empty string (None), add None to the 'first' set
+                    if None not in first[non_terminal]:
+                        first[non_terminal].add(None)
+                        changed = True
+
+    return first
+
+def follow_sets(productions, first_sets):
+    _, non_terminals = get_terminals_and_non_terminals(productions)
+    follow = {non_terminal: set() for non_terminal in non_terminals}
+    start_symbol = next(iter(non_terminals))
+    follow[start_symbol].add('$')
+
+    changed = True
+    while changed:
+        changed = False
+        for non_terminal in non_terminals:
+            for production in productions[non_terminal]:
+                for i, symbol in enumerate(production):
+                    if symbol in non_terminals:
+                        if i + 1 < len(production):
+                            next_symbol = production[i + 1]
+                            if next_symbol in non_terminals:
+                                added = len(follow[symbol])
+                                follow[symbol].update(first_sets[next_symbol] - {None})
+                                if len(follow[symbol]) != added:
+                                    changed = True
+                            else:
+                                if next_symbol not in follow[symbol]:
+                                    follow[symbol].add(next_symbol)
+                                    changed = True
+                        else:
+                            added = len(follow[symbol])
+                            follow[symbol].update(follow[non_terminal])
+                            if len(follow[symbol]) != added:
+                                changed = True
+
+    return follow
---
+++ LR.py
@@ -0,0 +1,70 @@
+class LR0Item:
+    def __init__(self, production, position,derived=False):
+        self.production = (production[0], tuple(production[1]))
+        self.position = position
+        self.derived = derived
+        
+
+    def __repr__(self):
+        return f'{self.production[0]} -> {" ".join(self.production[1][:self.position]) + "•" + " ".join(self.production[1][self.position:])}'
+
+    def __eq__(self, other):
+        return self.production == other.production and self.position == other.position
+
+    def __hash__(self):
+        return hash((self.production, self.position))
+    
+    def closure(items, productions):
+        new_items = set(items)
+        changed = True
+        while changed:
+            changed = False
+            for item in list(new_items):
+                if item.position < len(item.production[1]) and item.production[1][item.position] in productions:
+                    non_terminal = item.production[1][item.position]
+                    for production in productions[non_terminal]:
+                        new_item = LR0Item((non_terminal, production), 0, True)
+                        if new_item not in new_items:
+                            new_items.add(new_item)
+                            changed = True
+        return new_items
+
+    def goto(items, symbol, productions):
+        next_items = set()
+        for item in items:
+            if item.position < len(item.production[1]) and item.production[1][item.position] == symbol:
+                next_items.add(LR0Item(item.production, item.position + 1))
+        return closure(next_items, productions)
+
+
+    def canonical_collection(productions):
+        start_symbol = list(productions.keys())[0] + "'"
+        items = LR0Item((start_symbol, [list(productions.keys())[0]]), 0)
+        states = [closure({items}, productions)]
+        transitions = []
+        queue = [0]
+
+        while queue:
+            state_idx = queue.pop(0)
+            state = states[state_idx]
+
+            for symbol in set(sym for item in state for sym in item.production[1][item.position:item.position + 1]):
+                next_state = goto(state, symbol, productions)
+                if not next_state:
+                    continue
+                if next_state not in states:
+                    states.append(next_state)
+                    queue.append(len(states) - 1)
+                next_state_idx = states.index(next_state)
+                transitions.append((state_idx, symbol, next_state_idx))
+
+        # Add accept state
+        accept_state = len(states)
+        for i, state in enumerate(states):
+            for item in state:
+                if item.production[0] == start_symbol and item.position == len(item.production[1]) and item.derived == False:
+                    transitions.append((i, '$', accept_state))
+                    break
+        states.append(set())
+
+        return states, transitions
